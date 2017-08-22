0cdd7567799903e52cdc970cd8009d9fc08d5825
Abijit Suresh Geetha Ram <abijit.suresh@outlook.com>
2017-08-22 03:46:41 -0500

    Expense Manager - MVC EF - Final Commit.

--- ExpenseManagerMVC/Controllers/IncomesController.cs
+++ ExpenseManagerMVC/Controllers/IncomesController.cs
@@ -12,102 +12,102 @@ using ExpenseManager.DAL.Repositories;
 
 namespace ExpenseManagerMVC.Controllers
 {
-    public class ExpensesController : Controller
+    public class IncomesController : Controller
     {
         //private ExpenseManagerDataContext db = new ExpenseManagerDataContext();
-        private ExpenseRepository expenseRepository = new ExpenseRepository();
+
+        private IncomeRepository incomeRepository = new IncomeRepository();
         private UserRepository userRepository = new UserRepository();
 
-        // GET: Expenses
-        
+        // GET: Incomes
         public ActionResult Index()
         {
-            return View(expenseRepository.GetAll());
+            return View(incomeRepository.GetAll());
         }
 
-        // GET: Expenses/Details/5
+        // GET: Incomes/Details/5
         public ActionResult Details(int id)
         {
-            Expense expense = expenseRepository.GetByID(id);
-            if (expense == null)
+            Income income = incomeRepository.GetByID(id);
+            if (income == null)
             {
                 return HttpNotFound();
             }
-            return View(expense);
+            return View(income);
         }
 
-        // GET: Expenses/Create
+        // GET: Incomes/Create
         public ActionResult Create()
         {
             ViewBag.UserID = new SelectList(userRepository.GetUsers(), "UserID", "Name");
             return View();
         }
 
-        // POST: Expenses/Create
+        // POST: Incomes/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ExpenseID,ExpenseName,ExpenseAmount,UserID")] Expense expense)
+        public ActionResult Create([Bind(Include = "IncomeID,IncomeName,IncomeAmount,UserID")] Income income)
         {
             if (ModelState.IsValid)
             {
-                expenseRepository.Add(expense);
-                expenseRepository.SaveChanges();
+                incomeRepository.Add(income);
+                incomeRepository.SaveChanges();
                 return RedirectToAction("Index");
             }
 
-            ViewBag.UserID = new SelectList(userRepository.GetUsers(), "UserID", "Name", expense.UserID);
-            return View(expense);
+            ViewBag.UserID = new SelectList(userRepository.GetUsers(), "UserID", "Name", income.UserID);
+            return View(income);
         }
 
-        // GET: Expenses/Edit/5
+        // GET: Incomes/Edit/5
         public ActionResult Edit(int id)
         {
-            Expense expense = expenseRepository.GetByID(id);
-            if (expense == null)
+            Income income = incomeRepository.GetByID(id);
+            if (income == null)
             {
                 return HttpNotFound();
             }
-            ViewBag.UserID = new SelectList(userRepository.GetUsers(), "UserID", "Name", expense.UserID);
-            return View(expense);
+            ViewBag.UserID = new SelectList(userRepository.GetUsers(), "UserID", "Name", income.UserID);
+            return View(income);
         }
 
-        // POST: Expenses/Edit/5
+        // POST: Incomes/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ExpenseID,ExpenseName,ExpenseAmount,UserID")] Expense expense)
+        public ActionResult Edit([Bind(Include = "IncomeID,IncomeName,IncomeAmount,UserID")] Income income)
         {
             if (ModelState.IsValid)
             {
-                expenseRepository.Update(expense);
-                expenseRepository.SaveChanges();
+                incomeRepository.Update(income);
+                incomeRepository.SaveChanges();
                 return RedirectToAction("Index");
             }
-            ViewBag.UserID = new SelectList(userRepository.GetUsers(), "UserID", "Name", expense.UserID);
-            return View(expense);
+            ViewBag.UserID = new SelectList(userRepository.GetUsers(), "UserID", "Name", income.UserID);
+            return View(income);
         }
 
-        // GET: Expenses/Delete/5
+        // GET: Incomes/Delete/5
         public ActionResult Delete(int id)
         {
-            Expense expense = expenseRepository.GetByID(id);
-            if (expense == null)
+            Income income = incomeRepository.GetByID(id);
+            if (income == null)
             {
                 return HttpNotFound();
             }
-            return View(expense);
+            return View(income);
         }
 
-        // POST: Expenses/Delete/5
+        // POST: Incomes/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
-        {            
-            expenseRepository.Delete(id);
-            expenseRepository.SaveChanges();
+        {
+            incomeRepository.Delete(id);
+            incomeRepository.SaveChanges();
             return RedirectToAction("Index");
         }
 
@@ -115,7 +115,7 @@ namespace ExpenseManagerMVC.Controllers
         {
             if (disposing)
             {
-                expenseRepository.Dispose();
+                incomeRepository.Dispose();
             }
             base.Dispose(disposing);
         }
